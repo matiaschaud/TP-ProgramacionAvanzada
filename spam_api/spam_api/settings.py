ff7ac312df115ff4681cd1e121a2677d5f06ae8a
Ubuntu <matiaschaud@gmail.com>
2021-02-24 15:03:59 +0000

    WSGI funcionando

--- spam_api/spam_api/settings.py
+++ spam_api/spam_api/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for spam_api project.
 
@@ -11,6 +12,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import json
+
+with open('/etc/config.json') as config_file:
+    config = json.load(config_file)
+    
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f6d!ay+39i3xv#qzg3seiy4(e6bk=c*hg!@%%36trzj9j%(pjp'
+SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,6 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
 
 
